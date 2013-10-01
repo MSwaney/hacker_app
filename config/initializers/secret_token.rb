51092fe0db220ee681994013fc665954ae11e8dc
epicodus <student@epicodus.com>
2013-10-01 09:35:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacker::Application.config.secret_key_base = '2e11731e458f25985a7d870acefa72301e73a2387a9e8fa948d3dd40dae3ee13cf52a09ad7d22733367392aebbe64bb5593fd9ff17ba6637316f8f0cbd8ab343'
